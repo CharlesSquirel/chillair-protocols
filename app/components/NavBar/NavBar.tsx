fe16c5d665e3afa4517b47b82c8d092f26e73041
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2024-03-25 16:01:21 +0100

    Refactor: Change name of protocols, Delete signatures in in aside menu

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,10 +1,21 @@
 export default function NavBar() {
   return (
-    <nav className='pl-[300px]'>
-      <ul className='flex gap-7 text-secondary text-xl'>
-        <li className='hover:text-primary'>Wszystkie</li>
-        {Array.from({ length: 10 }, (_, index) => (
-          <li className='hover:text-primary'>{`Protokół ${index + 1}`}</li>
+    <nav className="pl-[300px]">
+      <ul className="flex gap-7 text-secondary text-xl">
+        {[
+          'Wszystkie',
+          'Zawory bezpieczeństwa',
+          'Awaria urządzenia',
+          'Agregaty',
+          'Centrale wentylacyjne',
+          'Klimatyzatory',
+          'Nawilżacze',
+          'Szafy',
+          'Szczelność',
+        ].map((str) => (
+          <li className="hover:text-primary" key={str}>
+            {str}
+          </li>
         ))}
       </ul>
     </nav>
