6ac04bd7082fc83e42a7f7b2419dfd5c80924f2b
CharlesSquirel <karol.wiewiorka@gmail.com>
2024-04-15 22:06:08 +0200

    feat: humidifier schema validation and pending status in SubmitFormbutton

--- app/components/SubmitFormButton/SubmitFormButton.tsx
+++ app/components/SubmitFormButton/SubmitFormButton.tsx
@@ -1,14 +1,20 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
 interface SubmitButtonProps {
   label: string;
 }
 
 export default function SubmitFormButton({ label }: SubmitButtonProps) {
+  const { pending } = useFormStatus();
   return (
     <button
       type="submit"
       className="btn btn-wide self-end rounded-md bg-primary text-lg text-white hover:border hover:border-primary hover:bg-white hover:text-primary"
     >
       {label}
+      {pending && <p>...</p>}
     </button>
   );
 }
