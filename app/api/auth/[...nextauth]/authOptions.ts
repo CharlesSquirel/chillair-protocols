a8f9643cb7f6f4a60375c3fa06cd1d09a010a89f
Karol <108272338+CharlesSquirel@users.noreply.github.com>
2024-04-24 19:38:07 +0200

    Update authOptions.ts

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -1,12 +1,10 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcrypt";
 import { db } from "lib/db";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { UserCredentials } from "@/utils/types/payloads";
 import { NextApiRequest } from "next";
 
 const authOptions = {
-  adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
   },
