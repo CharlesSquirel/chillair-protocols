212017174ac3716d4a887025df8ab636fbc1b2f9
unknown <karol.wiewiorka@gmail.com>
2024-04-25 09:39:46 +0200

    Feat: Refactor for deployment

--- app/api/auth/[...nextauth]/authOptions.ts
+++
@@ -1,4 +0,0 @@
-import CredentialsProvider from "next-auth/providers/credentials";
-import { compare } from "bcrypt";
-import { UserCredentials } from "@/utils/types/payloads";
-import { NextApiRequest } from "next";
