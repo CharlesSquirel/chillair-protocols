deff68b045465ad57f7e3922e9607e33f38a2f13
unknown <karol.wiewiorka@gmail.com>
2024-04-25 09:34:18 +0200

    Refactor for route error with deployment

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -2,20 +2,3 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcrypt";
 import { UserCredentials } from "@/utils/types/payloads";
 import { NextApiRequest } from "next";
-
-const authOptions = {
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/",
-  },
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      async authorize(credentials, request) {},
-    }),
-  ],
-};
-
-export default authOptions;
