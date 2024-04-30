c4ca93408fa5eb9894c8678fbc019d4f19e85edb
unknown <karol.wiewiorka@gmail.com>
2024-04-30 10:31:44 +0200

    Feat: Add  ChillerValidationSchema

--- app/components/ChillerForm/ChillerForm.tsx
+++ app/components/ChillerForm/ChillerForm.tsx
@@ -1,5 +1,6 @@
 import { FormType } from "@/utils/types/form";
 import FormContainer from "../FormContainer/FormContainer";
+import { ChillerValidationSchema } from "@/utils/zod/chillerValidationSchema";
 
 interface ChillerFormProps {
   formType: FormType;
@@ -11,6 +12,7 @@ export default function ChillerForm({ formType }: ChillerFormProps) {
       formType={formType}
       title="Protokół przegląd agregatu chłodniczego z kontrolą szczelności"
       closeUrl="/dashboard/chillers"
+      validationSchema={ChillerValidationSchema}
     ></FormContainer>
   );
 }
