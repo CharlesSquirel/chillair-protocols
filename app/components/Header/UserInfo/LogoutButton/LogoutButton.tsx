7c487a3f1994ea24c85a90d70cf6375f72e9f9aa
unknown <karol.wiewiorka@gmail.com>
2024-05-09 15:28:51 +0200

    Feat: Configure clerk auth

--- app/components/Header/UserInfo/LogoutButton/LogoutButton.tsx
+++ app/components/Header/UserInfo/LogoutButton/LogoutButton.tsx
@@ -1,9 +1,12 @@
 import LogoutIcon from "@/assets/icons/LogoutIcon";
+import { SignOutButton } from "@clerk/nextjs";
 
 export default function LogoutButton() {
   return (
-    <button type="button" className="hidden border-0 bg-transparent lg:block">
-      <LogoutIcon className="h-11 w-11" />
-    </button>
+    <SignOutButton redirectUrl="/sign-in">
+      <button className="hidden border-0 bg-transparent lg:block">
+        <LogoutIcon className="size-11" />
+      </button>
+    </SignOutButton>
   );
 }
--- app/components/Header/UserInfo/UserInfo.tsx
+++ app/components/Header/UserInfo/UserInfo.tsx
@@ -1,12 +1,20 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
 import LogoutButton from "./LogoutButton/LogoutButton";
+import { SignOutButton } from "@clerk/nextjs";
 
 export default function UserInfo() {
+  const { user } = useUser();
+  console.log(user);
   return (
     <div className="flex gap-6">
       <div className="flex flex-col text-secondary">
         <p className="text-lg lg:text-2xl">Witaj !</p>
-        <p className="text-xl font-semibold lg:text-2xl">Jan Kowalski</p>
+        <p className="text-xl font-semibold lg:text-2xl">
+          {user?.firstName} {user?.lastName}
+        </p>
       </div>
+
       <LogoutButton />
     </div>
   );
