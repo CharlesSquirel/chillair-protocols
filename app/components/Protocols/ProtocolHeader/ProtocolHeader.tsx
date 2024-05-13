3b419c5abe0abe284c63bbd338bf167d860d0585
CharlesSquirel <karol.wiewiorka@gmail.com>
2024-05-13 20:52:27 +0200

    Fixing: Fixing edit valve

--- app/components/Protocols/ProtocolHeader/ProtocolHeader.tsx
+++ app/components/Protocols/ProtocolHeader/ProtocolHeader.tsx
@@ -18,6 +18,7 @@ export default function ProtocolHeader({ title, id }: ProtocolHeaderProps) {
     <header className="relative mr-0 flex items-center justify-between md:mr-[170px] print:mr-0">
       <ProtocolTitle title={title} />
       <Image
+        priority
         src={logoImg}
         alt="Chillair logo"
         className="h-[94px] w-[256px] scale-[0.8] min-[770px]:scale-[.9] lg:scale-100 print:translate-x-[50px] print:scale-50"
